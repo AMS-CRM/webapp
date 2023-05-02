d25c57a9ecb4bae6e9fe898e8a9cee5b53a40ba7
Shivdeep Singh <shivdeepsingh@ShivdeesMacBook.ht.home>
2023-05-01 23:28:25 -0400

    Dash desing 1

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,32 +8,95 @@ import {
   Group,
   Button,
   BackgroundImage,
+  Flex,
+  Stack,
+  Badge,
 } from "@mantine/core";
 import fogg from "../assets/fogg-waiting-2.png";
 import "../dashboard.css";
 import WelcomeHeader from "../compenents/WelcomeHeader";
-import CardBg from "../assets/card.png";
+import CardImg from "../assets/card.png";
+import nfc from "../assets/nfc.png";
+import abstract from "../assets/abstract.png";
+import check from "../assets/completed.png";
 
 const Dashboard = () => {
   return (
     <Container size="lg" className="page-content">
       <WelcomeHeader />
       <Grid mt="lg">
-        <Grid.Col span={4}>
-          <Card shadow="xs" withBorder radius="md" backgroundColor="red">
-            <Grid>
-              <Grid.Col>
-                <Text weight={500} size="24px">
-                  $1200
-                </Text>
-                <Text color="dimmd"> Current Balance</Text>
+        <Grid.Col span={5}>
+          <Card
+            shadow="xs"
+            radius="md"
+            style={{
+              backgroundImage: `url(${CardImg})`,
+              backgroundSize: "cover",
+              height: "250px",
+            }}
+          >
+            <Grid justify="space-between" style={{ height: "100%" }}>
+              <Grid.Col span={5}>
+                <Stack justify="space-between" style={{ height: "100%" }}>
+                  <div>
+                    <Text color="#fff" weight="bold" size="36px" p="0" m="0">
+                      $1200
+                    </Text>
+                    <Text color="#fff" weight={500} p="0">
+                      Current Balance
+                    </Text>
+                  </div>
+                  <Group spacing="xs">
+                    <Text
+                      size="26px"
+                      weight="bold"
+                      color="white"
+                      pr="0"
+                      mt="7px"
+                      mr="0"
+                    >
+                      CREW
+                    </Text>
+                    <img src={nfc} width="36px" />
+                  </Group>
+                </Stack>
               </Grid.Col>
-              <Grid.Col>
-                <Button variant="light" color="blue.9">
-                  Fund Account
-                </Button>
+              <Grid.Col span={4}>
+                <Stack justify="flex-end" h="100%">
+                  <Button color="blue.9" variant="white" shadow="md">
+                    Add Funds
+                  </Button>
+                </Stack>
               </Grid.Col>
             </Grid>
+          </Card>
+        </Grid.Col>
+        <Grid.Col span={7}>
+          <Card radius="sm" h="250px" withBorder p="lg">
+            <Grid align="center" justify="center">
+              <Grid.Col span={12}>
+                <Text mt="10px" weight={600} color="gray.7" lin>
+                  PAYROLL STATUS
+                </Text>
+                <Card style={{ background: "#f9f9f9" }} mt="20px">
+                  <Group>
+                    <img src={check} width={40} />
+
+                    <Text weight={400}>
+                      All payroll are completed as per schedule
+                    </Text>
+                  </Group>
+                </Card>
+              </Grid.Col>
+            </Grid>
+            <Button
+              variant="light"
+              color="blue"
+              mt="20px"
+              style={{ float: "right" }}
+            >
+              View All
+            </Button>
           </Card>
         </Grid.Col>
       </Grid>
